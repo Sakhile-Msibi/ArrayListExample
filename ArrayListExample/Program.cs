b03482017bdffa5009cdbf1614ae200ee9e08482
Sakhile <Sakhile@codeplex.co.za>
2022-10-14 05:30:27 +0200

    Add project files.

---
+++ ArrayListExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(12.56);
+            arrayList.Add("GFG");
+            arrayList.Add(null);
+            arrayList.Add("G");
+            arrayList.Add(1234);
+
+            //arrayList.Sort();
+
+            foreach(var elements in arrayList)
+                Console.WriteLine(elements);
+        }
+    }
+}
